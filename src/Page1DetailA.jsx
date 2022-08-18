c407786175f3215c2ac97cda37685755e64ff4f4
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-08-18 17:09:10 +0900

    section5

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetalA = () => {
+  const { state } = useLocation();
+  console.log(state);
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>Page1DetalAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,9 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
+import { Page404 } from "../Page404";
 import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -26,8 +27,24 @@ export const Router = () => {
           </Switch>
         )}
       />
-      <Route path="/page2">
-        <Page2 />
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
       </Route>
     </Switch>
   );
